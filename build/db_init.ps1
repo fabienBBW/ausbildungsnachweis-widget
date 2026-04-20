d4b421b4f4b5709e883192c5ba25765be19b3856
Fabien Sonpar <ilovetrees.232@gmail.com>
2026-04-20 14:30:57 +0200

    build

--- build/db_init.ps1
+++ build/db_init.ps1
@@ -1,3 +1,2 @@
-cd c:/xampp
-mysql -u root < db_init.sql
+Get-Content "db_init.sql" | & "c:/xampp/mysql/bin/mysql.exe" -u root
 echo "Created user ausbildungsnachweis-widget. Created tables days and weeks."
--- build/db_init.sql
+++ build/db_init.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE ausbildungsnachweis;
-CREATE USER 'ausbildungsnachweis-widget'@'localhost' IDENTIFIED BY '600degrees';
+CREATE DATABASE IF NOT EXISTS ausbildungsnachweis;
+CREATE USER IF NOT EXISTS 'ausbildungsnachweis-widget'@'localhost' IDENTIFIED BY '600degrees';
 GRANT ALL PRIVILEGES ON ausbildungsnachweis.* TO 'ausbildungsnachweis-widget'@'localhost';
 FLUSH PRIVILEGES;
-CREATE TABLE IF NOT EXISTS days (
+CREATE TABLE IF NOT EXISTS ausbildungsnachweis.days (
             day_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             day_date TEXT NOT NULL,
             day_activities_json JSON NOT NULL,
             day_cw INT UNSIGNED NOT NULL,
             day_timestamp BIGINT UNSIGNED NOT NULL
         );
-CREATE TABLE IF NOT EXISTS weeks (
+CREATE TABLE IF NOT EXISTS ausbildungsnachweis.weeks (
             week_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             week_timestamp BIGINT UNSIGNED NOT NULL,
             week_cw INT UNSIGNED NOT NULL
